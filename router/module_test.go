383819281f88d831f5de1b261b866daece7d3457
Jacob Gadikian <jacobgadikian@gmail.com>
2023-04-12 05:17:50 +0700

    fix double import of transfer module in module_test.go

--- router/module_test.go
+++ router/module_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	apptypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	"github.com/golang/mock/gomock"
@@ -502,7 +501,7 @@ func TestOnRecvPacket_ForwardMultihopJSONNext(t *testing.T) {
 		setup.Mocks.TransferKeeperMock.EXPECT().Transfer(
 			sdk.WrapSDKContext(ctx),
 			msgTransfer1,
-		).Return(&apptypes.MsgTransferResponse{Sequence: 0}, nil),
+		).Return(&transfertypes.MsgTransferResponse{Sequence: 0}, nil),
 
 		setup.Mocks.IBCModuleMock.EXPECT().OnRecvPacket(ctx, packet2, senderAccAddr2).
 			Return(acknowledgement),
@@ -510,7 +509,7 @@ func TestOnRecvPacket_ForwardMultihopJSONNext(t *testing.T) {
 		setup.Mocks.TransferKeeperMock.EXPECT().Transfer(
 			sdk.WrapSDKContext(ctx),
 			msgTransfer2,
-		).Return(&apptypes.MsgTransferResponse{Sequence: 0}, nil),
+		).Return(&transfertypes.MsgTransferResponse{Sequence: 0}, nil),
 
 		setup.Mocks.IBCModuleMock.EXPECT().OnAcknowledgementPacket(ctx, packetFwd, successAck, senderAccAddr2).
 			Return(nil),
